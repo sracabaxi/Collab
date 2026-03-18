8cae42672b200f0af5009d8079fa26f2fb281bf8
sracabaxi <sir.acabaxi@gmail.com>
2026-03-18 18:33:18 -0300

    parte inicial

--- main.lua
+++ main.lua
@@ -13,8 +13,6 @@ function love.load()
 	player.posy = display.height * 0.25
 	player.velocityX = 0
 	player.velocityY = 0
-	player.colisionY = player.h + player.posy
-
 
 	plat = {}
 	plat.w = display.width * 0.05
@@ -26,30 +24,29 @@ function love.load()
 	
 	temp = 0
 	
-	function coliY()
-		if player.colisionY >= plat.posy then
-			player.posy = plat.posy - player.colisionY
-		end
-	end
-	
 end
 
 function love.update(dt)
 	
 	temp = temp + dt
 	
+	player.colisionY = player.posy + player.h
+	
 	player.posx = player.posx + player.velocityX * dt
 	player.posy = player.posy - player.velocityY * dt
 	
 	player.velocityY = player.velocityY - gravity * dt
 	
-	coliY()
+	if player.posy + player.h >= plat.posy then
+		player.posy = plat.posy - player.h
+		player.velocity = 0
+	end
 	
 end
 
 function love.draw()
 
-	love.graphics.rectangle("fill", player.posx, player.posy - player.velocityY, player.w, player.h, player.r)
+	love.graphics.rectangle("fill", player.posx, player.posy , player.w, player.h, player.r)
 	love.graphics.rectangle("line", plat.posx, plat.posy, plat.w, plat.h)
 	love.graphics.print(temp, 0, 0)
 
